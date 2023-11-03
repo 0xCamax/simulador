9e94b91260466554ca8ec5975a5dc85e7785079c
Camax <aebc95@gmail.com>
2023-11-02 22:24:44 -0700

    fin

--- JS/simulador.js
+++ JS/simulador.js
@@ -1,11 +1,27 @@
-console.log("conectado")
-
 function interesCompuesto() {
     let resultado = 0;
+
     let cantidad = parseFloat(prompt("Ingresa la cantidad para el calculo"));
+    if ((cantidad === null) || (cantidad === 0) || (cantidad <= 0)) {
+        alert("Ingresa una cantidad mayor a 0");
+        throw new Error("Ingresa una cantidad mayor a 0");
+    }
+
     let años = parseFloat(prompt("Años"));
+    if (años === null) {
+        alert("Ingresa un numero de años");
+        throw new Error("Ingresa un numero de años");
+    }
+    else if (años === 0) {
+        alert("No puede ser 0 años");
+        throw new Error("No puede ser 0 años");
+    }
     let interes = parseFloat(prompt("Porcentaje de interes"));
-    
+    if (interes === null){
+        alert("Ingresa un porcentraje de interes");
+        throw new Error("Ingresa un porcentraje de interes");
+    }
+
     for (let i = 0; i < años; i++){
     resultado = cantidad * (interes/100) + cantidad; 
     cantidad = resultado;
@@ -16,5 +32,7 @@ function interesCompuesto() {
 }
 
 interesCompuesto();
+
+
 
 
